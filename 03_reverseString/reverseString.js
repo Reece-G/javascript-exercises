53b0c24a84bbe986e393268d635014461d1d63bc
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2024-02-28 12:52:28 +0000

    Complete ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = [];
+    // for loop for each letter of string 
+    for (let i = 0; i < string.length; i ++) {
+        // attach the first letter to the end of stringArray using the length of string
+        let index = string.length - (i + 1);
+        stringArray[index] = string[i];
+    }
+    
+    let reversedString = "";
+    // for loop to iterate array and create reversed string
+    for (letter of stringArray) {
+        reversedString += letter;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
