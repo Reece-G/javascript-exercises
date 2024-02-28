fd1a68fcff952b8e9c4504d18acfcc4b76700206
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2024-02-28 17:20:45 +0000

    Complete ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
 
+    let first, last;
+    if (num1 > num2) {
+        first = num2;
+        last = num1;
+    } else {
+        first = num1;
+        last = num2;
+    }
+
+    let sumAll = (last - first + 1)/2 * (first + last);
+    return sumAll;
 };
 
 // Do not edit below this line
