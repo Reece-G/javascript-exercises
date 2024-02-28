96ed3dba35bf21c91fe76079e0db32b1c349fd49
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2024-02-28 15:56:38 +0000

    Complete ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    let removeCount = 0;
+    // iterate through the array 
+    for (let i = 0; i < array.length; i++) {
+        // iterate through ...items
+        for (let j = 0; j < items.length; j++) {
+            if (items[j] === array[i]) {
+                // remove item from array if it matches 
+                array.splice(i, 1);
+                i--;    // decrement i as the length of the array has decreased
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
