b361ffaf939f84f6596a65076a86e825327664d7
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2024-03-25 21:42:57 +0000

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  celsius = (number - 32) / (9/5);
+  roundCelsius = Math.round(celsius * 10);
+  return roundCelsius / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  fahren = (number * (9/5) + 32);
+  roundFahren = Math.round(fahren * 10);
+  return roundFahren / 10;
 };
 
 // Do not edit below this line
