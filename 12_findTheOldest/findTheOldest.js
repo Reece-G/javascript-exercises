3d9b74a9459f83ecd501b7d40b0ba7383ab1b627
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2025-02-14 11:34:27 +0000

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((lastPerson, person) => {
+        let lastPersonAge = (checkDead(lastPerson) - lastPerson.yearOfBirth)
+        let currentPersonAge = (checkDead(person) - person.yearOfBirth)
+        return lastPersonAge < currentPersonAge ? person : lastPerson
+    })
 };
+
+const checkDead = function(person) {
+    currentYear = new Date().getFullYear()
+    return person.yearOfDeath ? person.yearOfDeath : currentYear
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
